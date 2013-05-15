a80f24c9da14ad1bc211c1339a3a22854287c84d
Stefan Rusek <stefan@rusek.org>
2013-05-15 11:04:50 -0400

    Update espdy_stream
    * add {ok, noreply, State} response from cbmod:init()
    * pass messages from the stream to the cbmod

--- src/espdy_stream.erl
+++ src/espdy_stream.erl
@@ -61,7 +61,7 @@ headers_updated(Pid, Delta, NewMergedHeaders) ->
 %% gen_server callbacks
 
 init([StreamID, Pid, Headers, Mod, Opts]) ->
-    self() ! init_callback,
+    gen_server:cast(self(), init_callback),
 %%    Z = zlib:open(),
 %%    ok = zlib:deflateInit(Z),
     %%ok = zlib:deflateInit(Z, best_compression,deflated, 15, 9, default),
@@ -78,8 +78,66 @@ handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
+%% Called when we got a syn_stream for this stream.
+%% cb module is supposed to make and send the reply.
+handle_cast(init_callback, State) ->
+    case (State#state.mod):init(self(), State#state.headers, State#state.spdy_opts) of
+        %% In this case, the callback module provides the full response
+        %% with no need for this process to persist for streaming the body
+        %% so we can terminate this process after replying.
+        {ok, Headers, Body} when is_list(Headers), is_binary(Body) ->
+            %% se re-send this as a message to ourselves, because the callback
+            %% module may have dispatched other frames (eg, settings) before 
+            %% returning us this response:
+            send_response(self(), Headers, Body),
+            {noreply, State};
+        %% The callback module will call msg us the send_http_response
+        %% (typically from within the guts of cowboy_http_req, so that
+        %%  we can reuse the existing http API)
+        {ok, noreply} ->
+            %% TODO track state, set timeout on getting the response from CB
+            {noreply, State};
+        {ok, noreply, ModState} ->
+            {noreply, State#state{mod_state=ModState}};
+        %% CB module is going to stream us the body data, so we keep this process
+        %% alive until we get the fin packet as part of the stream.
+   %%%% {ok, Headers, stream, ModState} when is_list(Headers) ->
+   %%%%     NVPairsData = encode_name_value_pairs(Headers, State#state.z_context),
+   %%%%     StreamID = State#state.streamid,
+   %%%%     F = #cframe{type=?SYN_REPLY,
+   %%%%                 flags=0, 
+   %%%%                 length = 6 + byte_size(NVPairsData),
+   %%%%                 data= <<0:1, 
+   %%%%                         StreamID:31/big-unsigned-integer, 
+   %%%%                         0:16/big-unsigned-integer, %% UNUSED
+   %%%%                         NVPairsData/binary
+   %%%%                       >>},
+   %%%%     espdy_session:snd(State#state.pid, StreamID, F),
+   %%%%     {noreply, State#state{mod_state=ModState}};
+        %% CB module can't respond, because request is invalid
+        {error, not_http} ->
+            StreamID = State#state.streamid,
+            F = #spdy_rst_stream{ streamid=StreamID, statuscode=?PROTOCOL_ERROR },
+            espdy_session:snd(State#state.pid, StreamID, F),
+            {stop, normal, State}
+    end;
+
 handle_cast({send_frame, F}, State) ->
-    espdy_session:snd(State#state.pid, State#state.streamid, F),
+    StreamId = State#state.streamid,
+    FrameType = element(1, F),
+    F2 = case FrameType of
+        spdy_data ->
+            setelement(2, F, StreamId);
+        spdy_window_update ->
+            setelement(3, F, StreamId);
+        _ ->
+            setelement(4, F, StreamId)
+    end,
+    FullF = case FrameType of
+        spdy_data -> F;
+        _ -> setelement(2, F2, State#state.spdy_version)
+    end,
+    espdy_session:snd(State#state.pid, StreamId, FullF),
     {noreply, State};
 
 handle_cast(received_fin, State = #state{clientclosed=true}) ->
@@ -108,17 +166,17 @@ handle_cast({closed, Reason}, State) ->
     {stop, normal, State};
 
 %% part of streamed body
-handle_cast({data, Bin, false}, State) when is_binary(Bin) ->
+handle_cast({data, Bin}, State) when is_binary(Bin) ->
     F = #spdy_data{ streamid = State#state.streamid,
                     data=Bin},
     espdy_session:snd(State#state.pid, State#state.streamid, F),
     {noreply, State};
 
 %% last of streamed body
-handle_cast({data, Bin, true}, State) when is_binary(Bin) ->
+handle_cast({data, fin}, State) ->
     F = #spdy_data{ streamid = State#state.streamid,
                     flags=?DATA_FLAG_FIN,
-                    data=Bin},
+                    data= <<"">>},
     espdy_session:snd(State#state.pid, State#state.streamid, F),
     NewState = State#state{serverclosed=true},
     case both_closed(NewState) of
@@ -138,48 +196,9 @@ handle_cast({send_response, Headers, Body}, State) ->
                  {noreply, NewState}
     end.
     
-
-%% Called when we got a syn_stream for this stream.
-%% cb module is supposed to make and send the reply.
-handle_info(init_callback, State) ->
-    case (State#state.mod):init(self(), State#state.headers, State#state.spdy_opts) of
-        %% In this case, the callback module provides the full response
-        %% with no need for this process to persist for streaming the body
-        %% so we can terminate this process after replying.
-        {ok, Headers, Body} when is_list(Headers), is_binary(Body) ->
-            %% se re-send this as a message to ourselves, because the callback
-            %% module may have dispatched other frames (eg, settings) before 
-            %% returning us this response:
-            send_response(self(), Headers, Body),
-            {noreply, State};
-        %% The callback module will call msg us the send_http_response
-        %% (typically from within the guts of cowboy_http_req, so that
-        %%  we can reuse the existing http API)
-        {ok, noreply} ->
-            %% TODO track state, set timeout on getting the response from CB
-            {noreply, State};
-        %% CB module is going to stream us the body data, so we keep this process
-        %% alive until we get the fin packet as part of the stream.
-   %%%% {ok, Headers, stream, ModState} when is_list(Headers) ->
-   %%%%     NVPairsData = encode_name_value_pairs(Headers, State#state.z_context),
-   %%%%     StreamID = State#state.streamid,
-   %%%%     F = #cframe{type=?SYN_REPLY,
-   %%%%                 flags=0, 
-   %%%%                 length = 6 + byte_size(NVPairsData),
-   %%%%                 data= <<0:1, 
-   %%%%                         StreamID:31/big-unsigned-integer, 
-   %%%%                         0:16/big-unsigned-integer, %% UNUSED
-   %%%%                         NVPairsData/binary
-   %%%%                       >>},
-   %%%%     espdy_session:snd(State#state.pid, StreamID, F),
-   %%%%     {noreply, State#state{mod_state=ModState}};
-        %% CB module can't respond, because request is invalid
-        {error, not_http} ->
-            StreamID = State#state.streamid,
-            F = #spdy_rst_stream{ streamid=StreamID, statuscode=?PROTOCOL_ERROR },
-            espdy_session:snd(State#state.pid, StreamID, F),
-            {stop, normal, State}
-    end.
+handle_info(M, State) ->
+    {noreply, NewModState} = (State#state.mod):handle_info(M, State#state.mod_state),
+    {noreply, State#state{mod_state=NewModState}}.
 
 terminate(_Reason, _State) ->
     ok.
